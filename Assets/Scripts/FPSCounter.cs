559bdc589a30d2a56af27f4ec204e07f73631276
NuoC <nuoc@kth.se>
2018-04-08 14:54:15 +0200

    Created a blue neon lamp

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCounter : MonoBehaviour {
+	public int FPS {get; private set;}
+	public int AverageFPS{get; private set;}
+	
+	public int frameRange = 60;
+
+	int[] fpsBuffer;
+	int fpsBufferIndex;
+
+	void initializeBuffer() {
+		if (frameRange <= 0)
+			frameRange = 1;
+		fpsBuffer = new int[frameRange];
+		fpsBufferIndex = 0;
+	}
+	void Update(){
+		if (fpsBuffer == null || fpsBuffer.Length != frameRange) {
+			initializeBuffer();
+		}
+		updateBuffer();
+		calculateFPS();
+
+		// FPS = (int) (1f / Time.deltaTime);
+	}
+	void updateBuffer() {
+		fpsBuffer[fpsBufferIndex++] = (int) (1f / Time.deltaTime);
+		if (fpsBufferIndex >= frameRange) {
+			fpsBufferIndex = 0;
+		}
+	}
+
+	void calculateFPS() {
+		int sum = 0;
+		for (int i = 0; i<frameRange; i++){
+			sum += fpsBuffer[i];
+		}
+		AverageFPS = sum/frameRange;
+	}
+
+
+}
---
+++ Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(FPSCounter))]
+public class FPSDisplay : MonoBehaviour {
+	public Text fpsLabel;
+
+	FPSCounter fpsCounter;
+	void Awake(){
+		fpsCounter = GetComponent<FPSCounter>();
+	}	
+	void Update(){
+		// fpsLabel.text = Mathf.Clamp(fpsCounter.AverageFPS, 0, 999).ToString();
+		fpsLabel.text = fpsCounter.AverageFPS.ToString();
+	}
+}
